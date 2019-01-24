ffea1dfae71c90cc431dfcd363a81fe185aa0775
kliques <elemanhillary@gmail.com>
2019-01-24 04:46:00 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ export default ({children}) =>[
       <span></span>
     </div>
   </div>
-  <div id="navbarTransparent" className={styles.navbarMenu+" "+styles.left+" "+styles.tabs+" "+styles.isLeft+" "+styles.isMobile}>
+  <div id="navbarTransparent" className={styles.navbarMenu}>
     <div className={styles.navbarEnd}>
       <a className={styles.navbarItem+" "+styles1.navbarItem} href="#home">
       Home
